e529f454bbcdb304b77449fe17f8b7dd3a9793cf
PoJiC69 <cawancorp@gmail.com>
2026-02-23 06:36:45 +0700

    Create main.py as the main executable entry point

---
+++ main.py
@@ -0,0 +1,6 @@
+# Quantum Log Post Watch
+
+This is the main executable entry point for the Quantum Log Post Watch application. 
+
+if __name__ == '__main__':
+    print("Quantum Log Post Watch started!")
